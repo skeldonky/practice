4effd208a5e7696ecc1f20a4443a87b2712c12eb
skeldonky <75779367+skeldonky@users.noreply.github.com>
2024-12-31 10:15:43 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+    
+    while(n != 0){
+        answer = answer + n%10;
+        n = n/10;
+    }
+
+    return answer;
+}
